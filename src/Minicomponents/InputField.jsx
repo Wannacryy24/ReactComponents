a8362708f55c1ae1ad76d93b7b00286d9bb849e8
Sujal0007 <sujalvashisth1010@gmail.com>
2024-11-15 14:45:34 +0530

    added height and width , and onchange event functionality

--- src/Minicomponents/InputField.jsx
+++ src/Minicomponents/InputField.jsx
@@ -2,26 +2,38 @@ export default function Input({
   label,
   type,
   value,
-  onChange,
+  onChange = ()=>{},
   placeholder,
   name,
   id,
   required = false,
   className = "",
+  width='100%',
+  height='40px',
   ...props
 }) {
+
+    const handleChange = (event)=>{
+        if(onchange){
+            onChange(event);
+        }
+    }
+    
   return (
     <div className={className}>
-      {label && <label>{label}</label>}
+      {label && <label htmlFor={id || name}>{label}</label>}
+
       <input
         id={id || name}
         type={type}
         value={value}
-        onChange={onChange}
+        onChange={handleChange}
         placeholder={placeholder}
         name={name}
         required={required}
         className={className}
+        height={height}
+        width={width}
         {...props}
       />
     </div>
