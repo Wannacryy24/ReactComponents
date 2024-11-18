49d0195b53321c88a5a2cb4849f736e234a4abca
Mayank Verma <mayankverma.dev@gmail.com>
2024-11-18 15:47:16 +0530

    added switch cases for variant and image too

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
 import Button from "./Button"
+import Slider from "./Slider";
 
 function App() {
-  const handleClick = () => {
-    console.log('button Clicked');
-  }
-
-  const onMouseEnter = () => {
-    console.log('inside button');
-    
-  }
-
-  const onMouseLeave = () => {
-    console.log('outside button');
-  }
 
   return (
     <>
-      
+        <Button
+        className='Button-first'
+        // disabled={true}
+        variant='dashed'
+        // onClick={handleClick}
+        color={'primary'}
+        height={'50px'}
+        width={'200px'}
+        size={'30px'}
+        // onMouseEnter={onMouseEnter}
+        // onMouseLeave={onMouseLeave}
+      >
+        BUTTON
+      </Button>  
     </>
   )
 }
 
 export default App;
+
+// Environment variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 {/* <Button
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,30 +1,62 @@
 import React from 'react'
 import './Button.css'
 export default function Button(props) {
-   const {
-    children,
-    className,
-    disabled=false,
-    color='',
-    onClick,
-    variant='',
-    height='auto',
-    width='auto',
-    size='16px',
-    onMouseEnter,
-    onMouseLeave,
-    img,
-    src
-   } = props;
+  const {
+  children,
+  className,
+  disabled=false,
+  color='',
+  onClick,
+  variant='',
+  height='auto',
+  width='auto',
+  size='16px',
+  onMouseEnter,
+  onMouseLeave,
+  border='',
+  img,
+  src
+  } = props;
 
-  const buttonClass = `${className} ${variant?variant:""} ${color ? `btn-${color}` : ''}`;
-  
   const styleProps = {
     backgroundColor:color,
     height:height,
     width:width,
     fontSize:size
   };
+
+  let buttonClass = `${className} ${variant?variant:""} ${color ? `btn-${color}` : ''}`;
+  
+  switch(variant){
+      case 'primary':
+      buttonClass += 'btn-primary';  
+      styleProps.backgroundColor = color || 'blue';  
+      break;
+      case 'secondry':
+      buttonClass += 'btn-secondry';  
+      styleProps.backgroundColor = color || 'yellow';  
+      break;
+      case 'danger':
+      buttonClass += 'btn-danger';  
+      styleProps.backgroundColor = color || 'red';  
+      break;
+      case 'outlet':
+      buttonClass += 'btn-primary';  
+      styleProps.backgroundColor = color || 'black';  
+      break;
+      case 'solid':
+      buttonClass += 'btn-solid ';  
+      styleProps.backgroundColor = color || 'black';  
+      break;
+      case 'dashed':
+      buttonClass += 'btn-solid ';  
+      styleProps.border = 'dashed';  
+      break;
+  }
+
+  
+  
+  
 
   return (
     <button
@@ -35,7 +67,10 @@ export default function Button(props) {
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
         >{children}
+
         {img && <img src={src} alt="icon" style={{ marginRight: '8px', verticalAlign: 'middle' }} />}
+
+
     </button>
   )
 }
