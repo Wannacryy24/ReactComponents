be08eb8d829dfb81b93ad7ad79e00d491d0c8ba6
Mayank Verma <mayankverma.dev@gmail.com>
2024-11-20 10:57:04 +0530

    moved button component jsx and css file in a seprate folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import Button from "./Button"
-import Slider from "./Slider";
-
 function App() {
 
   return (
     <>
-        <Button
-        className='Button-first'
-        // disabled={true}
-        variant='dashed'
-        // onClick={handleClick}
-        color={'primary'}
-        height={'50px'}
-        width={'200px'}
-        size={'30px'}
-        // onMouseEnter={onMouseEnter}
-        // onMouseLeave={onMouseLeave}
-      >
-        BUTTON
-      </Button>  
+
     </>
   )
 }
