c1af7df39fba71586fbc823a5c670df9a83f4922
Mayank Verma <mayankverma.dev@gmail.com>
2024-11-21 11:38:58 +0530

    added circular progress bar and also added percentage in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,21 @@
 import Progress from "./ProgressBar/Progress";
-
 function App() {
-
   return (
     <>
         <Progress
-          value={20}
+          value={100}
           backgroundColor={'red'}
           percentage={true}
+          variant={'linear'}
         />
+         <Progress
+          variant="circular" 
+          value={75} 
+          radius={50}
+          strokeWidth={10} 
+          stroke="green"
+         />
     </>
   )
 }
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <Button
-        className='Button-first'
-        // disabled={true}
-        variant='outlined'
-        onClick={handleClick}
-        color={'primary'}
-        height={'50px'}
-        width={'200px'}
-        size={'30px'}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        BUTTON
-      </Button> */}
--- src/ProgressBar/Progress.jsx
+++ src/ProgressBar/Progress.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import './Progress.css'
-export default function Progress({height, width, className, value, backgroundColor,percentage=false}) {
+export default function Progress(props) {
+    let {   height,
+            width,
+            className,
+            value,
+            backgroundColor,
+            percentage=false,
+            variant='linear',
+            radius=49,
+            cx=50,
+            cy=50,
+            stroke='black',
+            strokeWidth=5
+        } = props;
+
     let fillValue = `${Math.min(Math.max(value,0),100)}%`;
+    let clampedValue = Math.min(Math.max(value, 0), 100);
+
+    let circumference = 2 * Math.PI*radius;
+    console.log(circumference);
+    
+    const strokeDashoffset = circumference - (circumference * value) / 100;
 
     var styleProp = {
         height: height || '16px',
@@ -11,22 +31,47 @@ export default function Progress({height, width, className, value, backgroundCol
     var fillStyle = {
         height:'100%',
         width: fillValue,
-        backgroundColor:backgroundColor || 'blue', 
+        backgroundColor:fillValue=='100%' ? 'green' : backgroundColor || 'blue', 
     }
 
   return (
     <>
-        <div
-            style={styleProp}
-            className={`progress-bar${className || ''}`}
-            aria-label='customized-Progress-bar'
-        >
-            <div 
-                style={fillStyle}
-                className='progress-bar-fill'
-            ></div>
-        </div>
-        {percentage && <p>{fillValue}</p>}
-    </>
-  )
+        {
+            variant === 'linear' ? 
+            <>
+                <div
+                    style={styleProp}
+                    className={`progress-bar${className || ''}`}
+                    aria-label='customized-Progress-bar'
+                >
+                    <div 
+                        style={fillStyle}
+                        className='progress-bar-fill'
+                    ></div>
+                </div>
+                {percentage && <p>{fillValue}</p>}
+                </>
+            :
+            (
+                <svg 
+                width={2 * radius} 
+                height={2 * radius} 
+                viewBox={`0 0 ${2 * radius} ${2 * radius}`}  
+                style={{ display: 'block', margin: '0 auto' }}     
+                >
+                    <circle 
+                        cx={cx} 
+                        cy={cy} 
+                        r={radius}
+                        fill={'transparent'}
+                        stroke={stroke} 
+                        strokeWidth={strokeWidth}
+                        strokeDasharray={circumference}
+                        strokeDashoffset={strokeDashoffset} //it shows progress of stroke  
+                        // style={{border:'2px solid black'}} 
+                    />
+                </svg>
+            )
+        }
+    </>)
 }
